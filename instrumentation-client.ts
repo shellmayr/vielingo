16b7fb6a08c17189accc4055cb38be655edb93c3
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-05-20 14:22:31 +0200

    save all replays

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -18,7 +18,7 @@ Sentry.init({
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 1.0,
 
   // Define how likely Replay events are sampled when an error occurs.
   replaysOnErrorSampleRate: 1.0,
